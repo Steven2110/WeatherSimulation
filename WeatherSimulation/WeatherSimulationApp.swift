633a782f0cff74a20e149de5b6358209847cb6cf
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-05-30 19:09:15 +0700

    Initial Commit

---
+++ WeatherSimulation/WeatherSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSimulationApp.swift
+//  WeatherSimulation
+//
+//  Created by Steven Wijaya on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
